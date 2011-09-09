506515763ead63b326306de0bf7920d14d5ab944
sly74fr <slymacdev@free.fr>
2011-09-09 11:31:05 +0000

    Added icon URLs.

--- smprun/src/fr/jmmc/smprun/HubPopulator.java
+++ smprun/src/fr/jmmc/smprun/HubPopulator.java
@@ -26,18 +26,21 @@ public class HubPopulator {
 
         md = new Metadata();
         md.setName("LITpro");
+        md.setIconUrl("http://www.jmmc.fr/images/litpro6464ws.jpg");
         capabilities = new SampCapability[]{SampCapability.LITPRO_START_SETTING};
         jnlpUrl = "http://jmmc.fr/~swmgr/LITpro/LITpro.jnlp";
         new ClientStub(md, capabilities, jnlpUrl).addObserver(new StubMonitor());
 
         md = new Metadata();
         md.setName("Aladin");
+        md.setIconUrl("http://aladin.u-strasbg.fr/aladin_large.gif");
         capabilities = new SampCapability[]{SampCapability.LOAD_VO_TABLE};
         jnlpUrl = "http://aladin.u-strasbg.fr/java/nph-aladin.pl?frame=get&id=aladin.jnlp";
         new ClientStub(md, capabilities, jnlpUrl).addObserver(new StubMonitor());
 
         md = new Metadata();
         md.setName("topcat");
+        md.setIconUrl("http://www.star.bris.ac.uk/~mbt/topcat/tc3.gif");
         capabilities = new SampCapability[]{SampCapability.LOAD_VO_TABLE};
         jnlpUrl = "http://www.star.bris.ac.uk/~mbt/topcat/topcat-full.jnlp";
         new ClientStub(md, capabilities, jnlpUrl).addObserver(new StubMonitor());
