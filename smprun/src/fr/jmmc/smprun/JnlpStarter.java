c397cf9d9cc5eb60a46eb35741f90daa6ca376a5
bourgesl <bourges.laurent@gmail.com>
2011-11-08 00:49:29 +0000

    return jobId to allow defered job cancellation

--- smprun/src/fr/jmmc/smprun/JnlpStarter.java
+++ smprun/src/fr/jmmc/smprun/JnlpStarter.java
@@ -5,14 +5,9 @@ package fr.jmmc.smprun;
 
 import fr.jmmc.jmcs.util.FileUtils;
 import fr.jmmc.smprun.stub.ClientStub;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import org.ivoa.util.runner.LocalLauncher;
 import org.ivoa.util.runner.RootContext;
-import org.ivoa.util.runner.process.ProcessContext;
 
 /**
  * Wrapper on http://code.google.com/p/vo-urp/ task runner
@@ -21,7 +16,7 @@ import org.ivoa.util.runner.process.ProcessContext;
 public class JnlpStarter {
 
     /** Class logger */
-    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(JnlpStarter.class.getName());
+    private static final java.util.logging.Logger _logger = java.util.logging.Logger.getLogger(JnlpStarter.class.getName());
     /** application identifier for LocalLauncher */
     public final static String APP_NAME = "JnlpStarter";
     /** user for LocalLauncher */
@@ -37,27 +32,27 @@ public class JnlpStarter {
      * Launch the Java WebStart application associated to the given client stub in another process.
      * 
      * @param client given client strub to launch the corresponding Jnlp application
-     * @return the job context
+     * @return the job context identifier
      * @throws IllegalStateException if the job can not be submitted to the job queue
      */
-    public static RootContext launch(final ClientStub client) throws IllegalStateException {
+    public static Long launch(final ClientStub client) throws IllegalStateException {
 
         final String jnlpUrl = client.getJnlpUrl();
         
-        if (logger.isLoggable(Level.INFO)) {
-            logger.info("launch: " + jnlpUrl);
+        if (_logger.isLoggable(Level.INFO)) {
+            _logger.info("launch: " + jnlpUrl);
         }
 
         // create the execution context without log file:
         final RootContext jobContext = LocalLauncher.prepareMainJob(APP_NAME, USER_NAME, FileUtils.getTempDir(), null);
 
         // command line: 'javaws -verbose <jnlpUrl>'
-        LocalLauncher.prepareChildJob(jobContext, TASK_NAME, new String[]{"javaws", "-verbose", jnlpUrl});
+        LocalLauncher.prepareChildJob(jobContext, TASK_NAME, new String[]{"javaws", "-Xnosplash", "-verbose", jnlpUrl});
 
         // puts the job in the job queue :
         // can throw IllegalStateException if job not queued :
         LocalLauncher.startJob(jobContext, client);
         
-        return jobContext;
+        return jobContext.getId();
     }
 }
