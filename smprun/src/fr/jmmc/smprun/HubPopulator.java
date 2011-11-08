cce7c42bf609161c9f82016f758cbacf4f39348d
bourgesl <bourges.laurent@gmail.com>
2011-11-08 00:51:14 +0000

    always use interfaces (Set / Map / List) instead of impl minor code refactoring

--- smprun/src/fr/jmmc/smprun/HubPopulator.java
+++ smprun/src/fr/jmmc/smprun/HubPopulator.java
@@ -17,6 +17,8 @@ import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import org.astrogrid.samp.Metadata;
 
 /**
@@ -30,12 +32,14 @@ public class HubPopulator {
     /** HubPopulator singleton */
     private static final HubPopulator INSTANCE = new HubPopulator();
     /* members */
+    /** all client stubs */
+    private final List<ClientStub> _clients = new ArrayList<ClientStub>();
     /** Client family  / client stub mapping */
     private EnumMap<ClientStubFamily, List<ClientStub>> _familyLists = new EnumMap<ClientStubFamily, List<ClientStub>>(ClientStubFamily.class);
-    /** all client stubs */
-    private final List<ClientStub> _clients = new ArrayList<ClientStub>(5);
-    private HashMap<String, ClientStub> _stubsMap = new HashMap<String, ClientStub>();
-    private HashSet<SampCapability> _mTypeSet = new HashSet<SampCapability>();
+    /** Client stub map keyed by application name */
+    private HashMap<String, ClientStub> _clientStubMap = new HashMap<String, ClientStub>();
+    /** SampCapability set */
+    private Set<SampCapability> _sampCapabilitySet = new HashSet<SampCapability>();
 
     /**
      * Return the HubPopulator singleton
@@ -50,61 +54,46 @@ public class HubPopulator {
      */
     private HubPopulator() {
         Metadata md;
-        String name;
-        SampCapability[] capabilities;
-        String jnlpUrl;
-        ClientStub client;
-        List<ClientStub> clients;
 
         // @TODO : Grab all this from the Web/OV
 
         // Note: Use Icon URL pointing to files extracted from Jar file (see resource package)
 
-        clients = new ArrayList<ClientStub>(3);
+        // JMMC client list:
+        final List<ClientStub> clientsJMMC = new ArrayList<ClientStub>(3);
 
         // --- ASPRO2 ---
         md = new Metadata();
-        name = "Aspro2";
-        md.setName(name);
+        md.setName("Aspro2");
         md.setIconUrl(extractResource("aspro2-6464.png")); // http://www.jmmc.fr/searchcal/images/aspro2-6464.png
-        jnlpUrl = "http://apps.jmmc.fr/~swmgr/Aspro2/Aspro2.jnlp";
-        capabilities = new SampCapability[]{SampCapability.LOAD_VO_TABLE};
-        _mTypeSet.addAll(Arrays.asList(capabilities));
-        client = new ClientStub(md, jnlpUrl, capabilities);
-        client.addObserver(new StubMonitor());
-        clients.add(client);
-        _stubsMap.put(name, client);
+
+        clientsJMMC.add(addClientStub(md,
+                "http://apps.jmmc.fr/~swmgr/Aspro2/Aspro2.jnlp",
+                new SampCapability[]{SampCapability.LOAD_VO_TABLE}));
 
         // --- SEARCHCAL ---
         md = new Metadata();
-        name = "SearchCal";
-        md.setName(name);
+        md.setName("SearchCal");
         md.setIconUrl(extractResource("searchcal-6464.png")); // http://apps.jmmc.fr/~sclws/SearchCal/AppIcon.png
-        jnlpUrl = "http://apps.jmmc.fr/~sclws/SearchCal/SearchCal.jnlp";
-        capabilities = new SampCapability[]{SampCapability.SEARCHCAL_START_QUERY};
-        _mTypeSet.addAll(Arrays.asList(capabilities));
-        client = new ClientStub(md, jnlpUrl, capabilities);
-        client.addObserver(new StubMonitor());
-        clients.add(client);
-        _stubsMap.put(name, client);
+
+        clientsJMMC.add(addClientStub(md,
+                "http://apps.jmmc.fr/~sclws/SearchCal/SearchCal.jnlp",
+                new SampCapability[]{SampCapability.SEARCHCAL_START_QUERY}));
 
         // --- LITPRO ---
         md = new Metadata();
-        name = "LITpro";
-        md.setName(name);
+        md.setName("LITpro");
         md.setIconUrl(extractResource("litpro-6464.png")); // http://www.jmmc.fr/images/litpro6464ws.jpg
-        jnlpUrl = "http://jmmc.fr/~swmgr/LITpro/LITpro.jnlp";
-        capabilities = new SampCapability[]{SampCapability.LITPRO_START_SETTING};
-        _mTypeSet.addAll(Arrays.asList(capabilities));
-        client = new ClientStub(md, jnlpUrl, capabilities);
-        client.addObserver(new StubMonitor());
-        clients.add(client);
-        _stubsMap.put(name, client);
 
-        _familyLists.put(ClientStubFamily.JMMC, clients);
-        _clients.addAll(clients);
+        clientsJMMC.add(addClientStub(md,
+                "http://jmmc.fr/~swmgr/LITpro/LITpro.jnlp",
+                new SampCapability[]{SampCapability.LITPRO_START_SETTING}));
 
-        clients = new ArrayList<ClientStub>(2);
+        // Update JMMC ClientStubFamily:
+        _familyLists.put(ClientStubFamily.JMMC, clientsJMMC);
+
+        // Generic client list:
+        final List<ClientStub> clientsGeneric = new ArrayList<ClientStub>(3);
 
         /*
          * Note: Following SAMP messages must not trigger application startup:
@@ -115,57 +104,83 @@ public class HubPopulator {
 
         // --- ALADIN ---
         md = new Metadata();
-        name = "Aladin";
-        md.setName(name);
+        md.setName("Aladin");
         md.setIconUrl(extractResource("aladin-6464.png")); // http://aladin.u-strasbg.fr/aladin_large.gif
-        jnlpUrl = "http://aladin.u-strasbg.fr/java/nph-aladin.pl?frame=get&id=aladin.jnlp";
-        capabilities = new SampCapability[]{SampCapability.LOAD_VO_TABLE,
-            /* SampCapability.POINT_COORDINATES, */
-            SampCapability.LOAD_FITS_IMAGE,
-            /* SampCapability.HIGHLIGHT_ROW, */
-            SampCapability.LOAD_FITS_TABLE, /* SampCapability.SELECT_LIST */};
-        _mTypeSet.addAll(Arrays.asList(capabilities));
-        client = new ClientStub(md, jnlpUrl, capabilities);
-        client.addObserver(new StubMonitor());
-        clients.add(client);
-        _stubsMap.put(name, client);
+
+        clientsGeneric.add(addClientStub(md,
+                "http://aladin.u-strasbg.fr/java/nph-aladin.pl?frame=get&id=aladin.jnlp",
+                new SampCapability[]{SampCapability.LOAD_VO_TABLE,
+                    /* SampCapability.POINT_COORDINATES, */
+                    SampCapability.LOAD_FITS_IMAGE,
+                    /* SampCapability.HIGHLIGHT_ROW, */
+                    SampCapability.LOAD_FITS_TABLE
+                /* SampCapability.SELECT_LIST */
+                }));
 
         // --- TOPCAT ---
         md = new Metadata();
-        name = "topcat";
-        md.setName(name);
+        md.setName("topcat");
         md.setIconUrl(extractResource("topcat-6464.png")); // "http://www.star.bris.ac.uk/~mbt/topcat/tc3.gif"
-        jnlpUrl = "http://www.star.bris.ac.uk/~mbt/topcat/topcat-full.jnlp";
-        capabilities = new SampCapability[]{
-            SampCapability.LOAD_VO_TABLE,
-            /* SampCapability.POINT_COORDINATES, */
-            /* SampCapability.HIGHLIGHT_ROW, */
-            SampCapability.LOAD_FITS_TABLE, /* SampCapability.SELECT_LIST */};
-        _mTypeSet.addAll(Arrays.asList(capabilities));
-        client = new ClientStub(md, jnlpUrl, capabilities);
-        client.addObserver(new StubMonitor());
-        clients.add(client);
-        _stubsMap.put(name, client);
 
-        _familyLists.put(ClientStubFamily.GENERAL, clients);
-        _clients.addAll(clients);
+        clientsGeneric.add(addClientStub(md,
+                "http://www.star.bris.ac.uk/~mbt/topcat/topcat-full.jnlp",
+                new SampCapability[]{SampCapability.LOAD_VO_TABLE,
+                    /* SampCapability.POINT_COORDINATES, */
+                    /* SampCapability.HIGHLIGHT_ROW, */
+                    SampCapability.LOAD_FITS_TABLE
+                /* SampCapability.SELECT_LIST */
+                }));
+
+        // Update GENERAL ClientStubFamily:
+        _familyLists.put(ClientStubFamily.GENERAL, clientsGeneric);
 
         _logger.info("configuration: " + _familyLists);
         _logger.info("clients:       " + _clients);
     }
 
-    public HashSet<SampCapability> getMTypeSet() {
-        return _mTypeSet;
+    /**
+     * Create a new Client Stub using given arguments and store it in collections
+     * @param md SAMP meta data
+     * @param jnlpUrl jnlp url
+     * @param capabilities samp capabilities
+     * @return client stub 
+     */
+    private ClientStub addClientStub(final Metadata md, final String jnlpUrl, final SampCapability[] capabilities) {
+        final ClientStub client = new ClientStub(md, jnlpUrl, capabilities);
+        client.addObserver(new StubMonitor());
+
+        _clients.add(client);
+        _clientStubMap.put(client.getApplicationName(), client);
+        _sampCapabilitySet.addAll(Arrays.asList(capabilities));
+
+        return client;
     }
 
-    public ClientStub getClientStub(String name) {
-        return _stubsMap.get(name);
+    /**
+     * Return the SampCapability set managed by client stubs
+     * @return SampCapability set
+     */
+    public Set<SampCapability> getSampCapabilitySet() {
+        return _sampCapabilitySet;
     }
 
-    public HashMap<String, ClientStub> getStubsMap() {
-        return _stubsMap;
+    /**
+     * Return the client stub map keyed by application name
+     * @return client stub map keyed by application name
+     */
+    public Map<String, ClientStub> getClientStubMap() {
+        return _clientStubMap;
     }
 
+    /**
+     * Return the client stub given its name
+     * @param name application name to match
+     * @return client stub or null if not found
+     */
+    public ClientStub getClientStub(final String name) {
+        return _clientStubMap.get(name);
+    }
+    
     /**
      * Return the client stubs per client family
      * @param family client family
