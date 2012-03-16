5ffabe700ebf7c7f11e23795829c0989af0fff80
sly74fr <slymacdev@free.fr>
2012-03-16 16:45:54 +0000

    Automatically tagged AppLauncher in version AL_V1_0b6 (mkfRepoUtil)

---
+++ smprun/src/fr/jmmc/smprun/preference/PreferenceKey.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.smprun.preference;
+
+/**
+ * Enumeration of all AppLauncher preference keys.
+ * @author Sylvain LAFRASSE
+ */
+public enum PreferenceKey {
+
+        FIRST_START_FLAG("first.start.flag");
+    
+    /** the preferenced value identifying token */
+    private final String _key;
+
+    /**
+     * Constructor
+     * @param key the preferenced value identifying token
+     */
+    PreferenceKey(String key) {
+        _key = key;
+    }
+
+    /**
+     * @return the preferenced value identifying token
+     */
+    @Override
+    public String toString() {
+        return _key;
+    }
+
+    /**
+     * For unit testing purpose only.
+     * @param args
+     */
+    public static void main(String[] args) {
+        for (PreferenceKey k : PreferenceKey.values()) {
+            System.out.println("Key '" + k.name() + "' = ['" + k + "'].");
+        }
+    }
+}
