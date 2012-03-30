965d4cd37de0dfc2240f39dfc7c86ffb40975011
sly74fr <slymacdev@free.fr>
2012-03-30 09:16:56 +0000

    Moved WindowCenterer to fr.jmmc.jmcs.gui.util.

--- src/fr/jmmc/smptest/AppLauncherTester.java
+++ src/fr/jmmc/smptest/AppLauncherTester.java
@@ -7,7 +7,7 @@ import fr.jmmc.jmcs.App;
 import fr.jmmc.jmcs.gui.MessagePane;
 import fr.jmmc.jmcs.gui.util.SwingSettings;
 import fr.jmmc.jmcs.gui.util.SwingUtils;
-import fr.jmmc.jmcs.gui.WindowCenterer;
+import fr.jmmc.jmcs.gui.util.WindowCenterer;
 import fr.jmmc.jmcs.network.interop.SampCapability;
 import fr.jmmc.jmcs.network.interop.SampManager;
 import fr.jmmc.jmcs.network.interop.SampMessageHandler;
