9961a53f51d8a8e4116e5e15dc811486ed73b63a
sly74fr <slymacdev@free.fr>
2012-08-28 09:40:47 +0000

    Automatically tagged AppLauncher in version AL_V1_1b1 (mkfRepoUtil)

---
+++ smprun/src/fr/jmmc/smprun/preference/ApplicationListSelectionView.java
@@ -0,0 +1,105 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.smprun.preference;
+
+import fr.jmmc.jmcs.data.preference.PreferencesException;
+import fr.jmmc.smprsc.data.list.ApplicationListSelectionPanel;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.JScrollPane;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Sylvain LAFRASSE
+ */
+public class ApplicationListSelectionView extends ApplicationListSelectionPanel implements Observer {
+
+    /** Logger - get from given class name */
+    private static final Logger _logger = LoggerFactory.getLogger(ApplicationListSelectionPanel.class.getName());
+    private final Preferences _preferences;
+
+    public ApplicationListSelectionView() {
+        super();
+        _preferences = Preferences.getInstance();
+    }
+
+    @Override
+    public void init() {
+
+        super.init();
+
+        update(null, null);
+        _preferences.addObserver(this);
+    }
+
+    @Override
+    public void update(Observable observable, Object parameter) {
+
+        List<String> selectedApplicationList = _preferences.getSelectedApplicationNames();
+        _logger.debug("Preferenced list of selected applications updated : {}", selectedApplicationList);
+
+        if (selectedApplicationList != null) {
+            setCheckedApplicationNames(selectedApplicationList);
+        }
+    }
+
+    /**
+     * @return A panel describing the application of given name.
+     * @param applicationName application name to describe.
+     */
+    public JScrollPane retrieveDescriptionPanelForApplication(String applicationName) {
+        fillApplicationDescriptionPane(applicationName);
+        return _descriptionScrollPane;
+    }
+
+    @Override
+    protected void checkedApplicationChanged(List<String> checkedApplicationList) {
+        _logger.debug("New list of SELECTED applications received : {}", checkedApplicationList);
+        saveStringListPreference(PreferenceKey.SELECTED_APPLICATION_LIST, checkedApplicationList);
+    }
+
+    @Override
+    protected boolean isApplicationBetaJnlpUrlInUse(String applicationName) {
+        return _preferences.isApplicationReleaseBeta(applicationName);
+    }
+
+    @Override
+    protected void betaApplicationChanged(List<String> betaApplicationList) {
+        _logger.debug("New list of BETA applications received : '{}'.", betaApplicationList);
+        saveStringListPreference(PreferenceKey.BETA_APPLICATION_LIST, betaApplicationList);
+    }
+
+    @Override
+    protected void applicationCliPathChanged(String applicationName, String cliPath) {
+        _logger.debug("New CLI path for application '{}' received : '{}'.", applicationName, cliPath);
+        _preferences.setApplicationCliPath(applicationName, cliPath);
+    }
+
+    @Override
+    protected String applicationCliPath(String applicationName) {
+
+        final String cliPath = _preferences.getApplicationCliPath(applicationName);
+        _logger.debug("Retrieved CLI path for application '{}' :  '{}'.", applicationName, cliPath);
+
+        if (cliPath == null) {
+            return "";
+        }
+        return cliPath;
+    }
+
+    private void saveStringListPreference(PreferenceKey preference, List<String> stringList) {
+
+        if (stringList == null) {
+            return;
+        }
+
+        try {
+            _preferences.setPreference(preference, stringList);
+        } catch (PreferencesException ex) {
+            _logger.error("PreferencesException :", ex);
+        }
+    }
+}
