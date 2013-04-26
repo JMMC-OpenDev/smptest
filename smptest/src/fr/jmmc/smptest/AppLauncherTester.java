4378271458c2fccbf704a822f27241fdc1df38a8
sly74fr <slymacdev@free.fr>
2013-04-26 12:39:42 +0000

    Automatically tagged AppLauncher in version AL_V1_1_2b1 from trunk  (mkfRepoUtil)

---
+++ smptest/src/fr/jmmc/smptest/AppLauncherTester.java
@@ -0,0 +1,116 @@
+/**
+ * *****************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ * ****************************************************************************
+ */
+package fr.jmmc.smptest;
+
+import fr.jmmc.jmcs.App;
+import fr.jmmc.jmcs.Bootstrapper;
+import fr.jmmc.jmcs.gui.component.MessagePane;
+import fr.jmmc.jmcs.gui.util.SwingUtils;
+import fr.jmmc.jmcs.gui.util.WindowUtils;
+import fr.jmmc.jmcs.network.interop.SampCapability;
+import fr.jmmc.jmcs.network.interop.SampManager;
+import fr.jmmc.jmcs.network.interop.SampMessageHandler;
+import javax.swing.JFrame;
+import org.astrogrid.samp.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * AppLauncherTester main class.
+ *
+ * @author Sylvain LAFRASSE
+ */
+public class AppLauncherTester extends App {
+
+    /** Logger */
+    private static final Logger _logger = LoggerFactory.getLogger(AppLauncherTester.class.getName());
+
+    /**
+     * Launch the AppLauncherTester application.
+     *
+     * @param args command-line options.
+     */
+    public AppLauncherTester(final String[] args) {
+        // Start whith no splash screen
+        super(args);
+    }
+
+    @Override
+    protected void initServices() {
+        // No op
+    }
+
+    @Override
+    protected void setupGui() {
+        // Start first the SampManager (connect to an existing hub or ___internalStart a new one)
+        // and check if it is connected to one Hub:
+        if (!SampManager.isConnected()) {
+            throw new IllegalStateException("Unable to connect to an existing hub or start an internal SAMP hub !");
+        }
+    }
+
+    @Override
+    protected void execute() {
+        // No op
+    }
+
+    /**
+     * Create SAMP Message handlers
+     */
+    @Override
+    protected void declareInteroperability() {
+
+        // Add fake handler to allow AppLauncher JNLP startup test routine
+        new SampMessageHandler(SampCapability.APPLAUNCHERTESTER_TRY_LAUNCH) {
+            /**
+             * Implements message processing
+             *
+             * @param senderId public ID of sender client
+             * @param message message with MType this handler is subscribed to
+             * @throws SampException if any error occurred while message processing
+             */
+            @Override
+            protected void processMessage(final String senderId, final Message message) {
+                if (_logger.isInfoEnabled()) {
+                    _logger.info("Received '{}' message from '{}' : '{}'.",
+                            this.handledMType(), senderId, message);
+                }
+                // Using invokeAndWait to be in sync with this thread :
+                // note: invokeAndWaitEDT throws an IllegalStateException if any exception occurs
+                SwingUtils.invokeAndWaitEDT(new Runnable() {
+                    /**
+                     * Initializes the SWING components with their actions in EDT
+                     */
+                    @Override
+                    public void run() {
+                        final JFrame frame = getFrame();
+                        WindowUtils.centerOnMainScreen(frame);
+                        frame.setVisible(false);
+                        MessagePane.showMessage("AppLauncher installation and first run went fine !", "Congratulation !");
+                        frame.setVisible(false);
+                        Bootstrapper.stopApp(0);
+                    }
+                });
+            }
+        };
+    }
+
+    @Override
+    protected void cleanup() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    /**
+     * Main entry point
+     *
+     * @param args command line arguments (open file ...)
+     */
+    @SuppressWarnings("ResultOfObjectAllocationIgnored")
+    public static void main(final String[] args) {
+        Bootstrapper.launchApp(new AppLauncherTester(args), false, true, false);
+    }
+}
+/*___oOo___*/
